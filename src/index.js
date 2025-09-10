7c5f9ee16729417a3d63214184313657c050b5b6
Kevyn <kevwsantos9@gmail.com>
2025-09-10 17:06:12 -0300

    Atualizando arquivo para jsx

--- src/index.js
+++
@@ -1,4 +0,0 @@
-// src/index.js
-export { default as AuthProvider } from './auth/AuthProvider';
-export { default as SessionGuard } from './auth/SessionGuard';
-export { useAuth } from './auth/useAuth';
