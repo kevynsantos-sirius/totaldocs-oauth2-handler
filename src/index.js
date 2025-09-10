dbeb66c6f39a4bf039befde1bd7272237dee9df1
Kevyn <kevwsantos9@gmail.com>
2025-09-10 17:26:15 -0300

    Criando index.js e apontando no package.json

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// src/index.js
+export { default as AuthProvider } from './auth/AuthProvider';
+export { default as SessionGuard } from './auth/SessionGuard';
+export { default as useAuth } from './auth/useAuth';
