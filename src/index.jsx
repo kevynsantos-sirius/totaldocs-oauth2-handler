a1cf8acd9679f59d1eee110b0f3cfa3f55db07fd
Kevyn <kevwsantos9@gmail.com>
2025-09-10 17:08:36 -0300

    Optando em exportar no main.jsx

--- src/index.jsx
+++
@@ -1,4 +0,0 @@
-// src/index.js
-export {default as AuthProvider} from './auth/AuthProvider';
-export { default as SessionGuard } from './auth/SessionGuard';
-export { default as useAuth } from './auth/useAuth';
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+// src/index.js
+export {default as AuthProvider} from './auth/AuthProvider';
+export { default as SessionGuard } from './auth/SessionGuard';
+export { default as useAuth } from './auth/useAuth';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
