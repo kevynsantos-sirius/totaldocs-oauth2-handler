23c390254ea01dda1f5d86a263232c42cc267343
Kevyn <kevwsantos9@gmail.com>
2025-09-11 14:42:35 -0300

    Fazendo teste retirando o browserRouter para evitar conflito

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 // src/App.jsx
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AuthProvider from "./auth/AuthProvider";
 import {getCallbackRoute} from "./CallbackRoute";
 
 export default function App() {
   return (
-    <BrowserRouter>
       <AuthProvider>
         <Routes>
           {getCallbackRoute()}
           <Route path="*" element={null} /> {/* nada visível na rota principal */}
         </Routes>
       </AuthProvider>
-    </BrowserRouter>
   );
 }
