7de1f14a6c62f41134800f23a977c3a5ba18cc48
Kevyn <kevwsantos9@gmail.com>
2025-09-16 11:16:55 -0300

    revertendo para testes a auto execução

--- src/pages/RootRedirect.tsx
+++ src/pages/RootRedirect.tsx
@@ -1,23 +1,25 @@
-import { useEffect, useRef } from "react";
+// src/pages/RootRedirect.tsx
+import { useEffect } from "react";
 import useAuth from "../auth/useAuth";
 
 interface RootRedirectProps {
+  /** rota padrão para onde redirecionar após login */
   main: string;
 }
 
 export default function RootRedirect({ main }: RootRedirectProps) {
   const { auth, checkLogin } = useAuth();
-  const ran = useRef(false); // evita rodar mais de uma vez
 
   useEffect(() => {
-    if (ran.current) return;
-    ran.current = true;
-
+    console.log(localStorage.getItem("lastPath") );
+    console.log(main);
     if (auth) {
+      // já autenticado → redireciona para rota principal
       const lastPath = localStorage.getItem("lastPath") || main;
       localStorage.removeItem("lastPath");
       window.location.replace(lastPath);
     } else {
+      // sem sessão → dispara login automático
       checkLogin(true);
     }
   }, [auth, checkLogin, main]);
