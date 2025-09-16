a4b80e533dd5fe84398c427c82ce2bdf811bfea9
Kevyn <kevwsantos9@gmail.com>
2025-09-16 16:10:13 -0300

    Adicionando direcionamento para rota de erro

--- src/pages/Callback.tsx
+++ src/pages/Callback.tsx
@@ -19,7 +19,7 @@ export default function Callback() {
       });
     } else {
       console.error("Callback sem código OAuth2!");
-      window.location.replace("/");
+      window.location.replace("/errorAuth");
     }
   }, [handleCallback]);
 
