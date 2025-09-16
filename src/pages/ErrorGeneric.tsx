3dd6bdd891c99a747049b5f3ea5790aa2ed77b8e
Kevyn <kevwsantos9@gmail.com>
2025-09-16 16:05:52 -0300

    corrigindo problema em css

--- src/pages/ErrorGeneric.tsx
+++ src/pages/ErrorGeneric.tsx
@@ -1,9 +1,21 @@
-import '../assets/css/error.css';
-
-export default function ErrorGeneric()
-{
-    return <div className='div_error'>
-        <h4>Ocorrou um erro inesperado!</h4>
+export default function ErrorGeneric() {
+  return (
+    <div>
+      <style>
+        {`
+          .div_error {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            text-align: center;
+          }
+        `}
+      </style>
+      <div className='div_error'>
+        <h4>Ocorreu um erro inesperado!</h4>
         <p>Contate o Administrador do sistema para mais detalhes.</p>
+      </div>
     </div>
+  );
 }
