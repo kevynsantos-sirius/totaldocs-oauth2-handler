bd242a9f1715ea5c27291a3fa179b246e67b2cfb
Kevyn <kevwsantos9@gmail.com>
2025-09-16 15:57:05 -0300

    Criando pagina generica de erro do oauth2

---
+++ src/pages/ErrorGeneric.tsx
@@ -0,0 +1,9 @@
+import '../assets/css/error.css';
+
+export default function ErrorGeneric()
+{
+    return <div className='div_error'>
+        <h4>Ocorrou um erro inesperado!</h4>
+        <p>Contate o Administrador do sistema para mais detalhes.</p>
+    </div>
+}
