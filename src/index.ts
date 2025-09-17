231717b1a12363f2792b35287c7f269c16207f2e
Kevyn <kevwsantos9@gmail.com>
2025-09-17 17:05:05 -0300

    Disponibilizando rota Callback

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@
 export { default as OAuth2SessionGuard } from './pages/OAuth2SessionGuard';
 export { default as authInterceptor } from './api/authInterceptor';
 export { default as AuthContext } from './auth/AuthContext';
+export { default as Callback } from './pages/Callback';
