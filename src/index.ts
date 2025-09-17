d8001d96f572d95f953eea3544b53f28919a6f91
Kevyn <kevwsantos9@gmail.com>
2025-09-17 18:21:21 -0300

    Removendo exportação

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 // src/index.ts
 export { default as OAuth2SessionGuard } from './pages/OAuth2SessionGuard';
 export { default as authInterceptor } from './api/authInterceptor';
-export { default as AuthProvider } from './auth/AuthContext';
 export { default as Callback } from './pages/Callback';
