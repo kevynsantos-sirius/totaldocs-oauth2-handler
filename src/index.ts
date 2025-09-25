882ce3354f5a7911d5a637cf0e1a2b484517689d
Kevyn <kevwsantos9@gmail.com>
2025-09-25 09:59:29 -0300

    exportando componente

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,4 @@ export { default as OAuth2SessionGuard } from './pages/OAuth2SessionGuard';
 export { default as authInterceptor } from './api/authInterceptor';
 export * from './utils/pkce';
 export { getTokenSession } from './utils/storageSession';
+export { default as AppInitializer } from './initializer/AppInitializer';
